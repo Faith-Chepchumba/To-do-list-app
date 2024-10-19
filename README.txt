b4de0efba34575242eba2c39459f63b3ad10a0cf
Faith Chepchumba Cheruiyot <cfaithterer@gmail.com>
2024-10-18 22:14:00 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,22 @@
+# To-Do List App
+
+## Overview
+
+A simple To-Do List app that allows users to add and view tasks.
+
+## Features
+
+- Add tasks to the list
+- Simple and intuitive interface
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## How to Run
+
+1. Open index.html in a web browser.
+
+2. Add tasks using the input field.
---
+++ script.js
@@ -0,0 +1,21 @@
+function addTask() {
+
+    const taskInput = document.getElementById('taskInput');
+    
+    const taskList = document.getElementById('taskList');
+    
+    const taskText = taskInput.value;
+    
+    if (taskText) {
+    
+    const li = document.createElement('li');
+    
+    li.textContent = taskText;
+    
+    taskList.appendChild(li);
+    
+    taskInput.value = '';
+    
+    }
+    
+    }
